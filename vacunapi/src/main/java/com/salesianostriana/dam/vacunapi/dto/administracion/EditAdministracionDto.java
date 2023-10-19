19f3de34080d6466a23544dabbae58496dca9ea7
Fran Ruiz <fjavierrule@gmail.com>
2023-10-20 01:38:13 +0200

    Request POST no conseguido

--- vacunapi/src/main/java/com/salesianostriana/dam/vacunapi/dto/administracion/EditAdministracionDto.java
+++ vacunapi/src/main/java/com/salesianostriana/dam/vacunapi/dto/administracion/EditAdministracionDto.java
@@ -1,50 +1,44 @@
-package com.salesianostriana.dam.vacunapi.dto.administracion;
-
-import com.fasterxml.jackson.annotation.JsonView;
-import com.salesianostriana.dam.vacunapi.View.AdministracionView;
-import com.salesianostriana.dam.vacunapi.dto.calendario.EditCalendarioDto;
-import com.salesianostriana.dam.vacunapi.dto.calendario.GetCalendarioDto;
-import com.salesianostriana.dam.vacunapi.dto.paciente.GetPacienteFindAll;
-import com.salesianostriana.dam.vacunapi.modelo.Administracion;
-import com.salesianostriana.dam.vacunapi.modelo.Calendario;
-import com.salesianostriana.dam.vacunapi.modelo.Paciente;
-import com.salesianostriana.dam.vacunapi.modelo.Vacuna;
-
-import java.time.LocalDate;
-
-public record EditAdministracionDto(
-
-//        @JsonView({AdministracionView.administracionCreate.class})
+//package com.salesianostriana.dam.vacunapi.dto.administracion;
+//
+//import com.salesianostriana.dam.vacunapi.dto.calendario.GetCalendarioDto;
+//import com.salesianostriana.dam.vacunapi.dto.paciente.GetPacienteFindAll;
+//import com.salesianostriana.dam.vacunapi.modelo.Administracion;
+//
+//import com.salesianostriana.dam.vacunapi.modelo.Paciente;
+//
+//
+//import java.time.LocalDate;
+//
+//public record EditAdministracionDto(
+//
 //        Long id,
 //
-//        @JsonView({AdministracionView.administracionCreate.class})
 //        LocalDate fecha,
 //
-//        @JsonView({AdministracionView.administracionCreate.class})
 //        int edadAlAdministrar,
 //
-//        @JsonView({AdministracionView.administracionCreate.class})
 //        String notas,
 //
-//        @JsonView({AdministracionView.administracionCreate.class})
-//        GetPacienteFindAll pacienteFindAll
-
-        //GetCalendarioDto getCalendario
-) {
-
-//            public static EditAdministracionDto of (Administracion a){
+//        GetPacienteFindAll paciente,
 //
-//                Paciente p = a.getPaciente();
+//        GetCalendarioDto calendario
 //
-//                return new GetAdministracionDto(
-//                                a.getId(),
-//                                a.getFecha(),
-//                                a.getEdadAlAdministrar(),
-//                                a.getNotas(),
-//                                GetPacienteFindAll.of(a.getPaciente())
-//                );
-
-
-}
-
-
+//
+//
+//) {
+//
+//    public static EditAdministracionDto of (Administracion a){
+//
+//        return new EditAdministracionDto(
+//                a.getId(),
+//                a.getFecha(),
+//                a.getEdadAlAdministrar(),
+//                a.getNotas(),
+//                GetPacienteFindAll.of(a.getPaciente()),
+//                GetCalendarioDto.of(a.getMomento())
+//        );
+//    }
+//
+//}
+//
+//
